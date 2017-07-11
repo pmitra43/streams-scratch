c290b61d4f1d6bb8cf686f55124e101e213a55d9
Priyam Mitra <pmitra43@gmail.com>
2017-07-11 21:25:38 +0530

    Creating stream to produce the data points to topic:'anomaly-count' when cpu is more than 80

--- src/main/java/hello/HelloKafkaStreams.java
+++ src/main/java/hello/HelloKafkaStreams.java
@@ -1,25 +1,44 @@
 package hello;
 
+import hello.models.CpuUsage;
+import hello.serdes.CpuUsageDeserializer;
+import hello.serdes.CpuUsageSerializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.KeyValueMapper;
-import org.apache.kafka.streams.kstream.ValueMapper;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
 
-import java.util.Arrays;
-import java.util.Locale;
 import java.util.Properties;
 
 public class HelloKafkaStreams {
     public static void main(String[] args) {
         Properties settings = new Properties();
-        settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "helgeelo-kafka-streams");
+        settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "anomaly-kafka-streams");
         settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        CpuUsageSerializer cpuUsageSerializer = new CpuUsageSerializer();
+        CpuUsageDeserializer cpuUsageDeserializer = new CpuUsageDeserializer();
+        StringSerializer stringSerializer = new StringSerializer();
+        StringDeserializer stringDeserializer = new StringDeserializer();
+
+        Serde<String> stringSerde = Serdes.serdeFrom(stringSerializer, stringDeserializer);
+        Serde<CpuUsage> cpuUsageSerde = Serdes.serdeFrom(cpuUsageSerializer, cpuUsageDeserializer);
+
+        KStreamBuilder builder = new KStreamBuilder();
+
+        KStream<String, CpuUsage> rawStream = builder.stream(stringSerde, cpuUsageSerde, "hardware-usage")
+                .filter((k, cpu) -> cpu.getCpu()>80.0);
+        rawStream.to(stringSerde, cpuUsageSerde, "anomaly-count");
+        System.out.println("Starting Anomaly detection(rule based) Example");
+
+        KafkaStreams kafkaStreams = new KafkaStreams(builder, settings);
+        kafkaStreams.start();
+        System.out.println("Streaming started");
     }
 }
