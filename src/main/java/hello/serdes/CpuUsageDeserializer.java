7fef0aa64591a8d9154f7ac103a9aa7339e6f5fb
Priyam Mitra <pmitra43@gmail.com>
2017-07-11 16:13:17 +0530

    Creating Serializer Deserializer for CpuUsage

---
+++ src/main/java/hello/serdes/CpuUsageDeserializer.java
@@ -0,0 +1,32 @@
+package hello.serdes;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.models.CpuUsage;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class CpuUsageDeserializer implements Deserializer {
+    @Override
+    public void configure(Map map, boolean b) {
+
+    }
+
+    @Override
+    public CpuUsage deserialize(String s, byte[] bytes) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        CpuUsage cpuUsage = null;
+        try {
+            cpuUsage = objectMapper.readValue(bytes, CpuUsage.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cpuUsage;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ src/main/java/hello/serdes/CpuUsageSerializer.java
@@ -0,0 +1,32 @@
+package hello.serdes;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.models.CpuUsage;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class CpuUsageSerializer implements Serializer {
+    @Override
+    public void configure(Map map, boolean b) {
+
+    }
+
+    @Override
+    public byte[] serialize(String s, Object o) {
+        byte[] retVal = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            retVal = objectMapper.writeValueAsString(o).getBytes();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return retVal;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
