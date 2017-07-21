0d53f0e432405ee2dc24923aa6ba1b1c4d255137
Priyam Mitra <pmitra43@gmail.com>
2017-07-21 14:49:05 +0530

    Adding commit interval value to be 1000

--- src/main/java/hello/HelloKafkaStreams.java
+++ src/main/java/hello/HelloKafkaStreams.java
@@ -20,6 +20,7 @@ public class HelloKafkaStreams {
         settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         settings.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         settings.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        settings.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "1000");
 
         Predicate<String, CpuUsage> normalPredicate = (k, cpu) -> cpu.getCpu() <= 80;
         Predicate<String, CpuUsage> anomalyPredicate = (k, cpu) -> cpu.getCpu() > 80;
